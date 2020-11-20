d7ce88e603862835697767c4eb283ac615521334
kisakyegordon <gordon56692@gmail.com>
2020-11-19 22:05:51 -0600

    adds authentication, login, signout

--- src/App.js
+++ src/App.js
@@ -1,21 +1,51 @@
+import React, { useEffect } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { useStateValue } from "./StateProvider";
+import { auth } from "./firebase";
 import "./App.css";
 import Header from "./Header";
 import Home from "./Home";
+import Login from "./Login";
 import Checkout from "./Checkout";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
+  const [{}, dispatch] = useStateValue();
+
+  useEffect(() => {
+    // will only run once when the app component loads...
+
+    auth.onAuthStateChanged((authUser) => {
+      console.log("User: ", authUser);
+
+      if (authUser) {
+        // the user just loggedin / the user was loggedin
+        dispatch({
+          type: "SET_USER",
+          user: authUser
+        });
+      } else {
+        // the user is logged out
+        dispatch({
+          type: "SET_USER",
+          user: null
+        });
+      }
+    });
+  }, []);
   return (
     // BEM convetion
     <Router>
       <div className="app">
-        <Header />
-
         <Switch>
+          <Route path="/login">
+            <Login />
+          </Route>
           <Route path="/checkout">
+            <Header />
             <Checkout />
           </Route>
           <Route path="/">
+            <Header />
             <Home />
           </Route>
         </Switch>
